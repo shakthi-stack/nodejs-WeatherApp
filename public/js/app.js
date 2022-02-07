a47091ca328e2d45a51f561cde2e84338695c83c
shakthi-stack <shakthisaravanan1@outlook.com>
2022-02-07 12:11:38 +0530

    app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@ form.addEventListener('submit',(e)=>{
     // messageOne.textContent = "";
     // messageTwo.textContent = "";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then(
+    fetch(`/weather?address=${location}`).then(
     (res)=>{
         res.json().then((data)=>{
             if(data.error){
--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ const hbs = require('hbs')
 const geoCode = require('./utils/geoCode');
 const forecast = require('./utils/forecast');
 
-const app = express()
-
+const app = express();
+const port = process.env.PORT || 3000;
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -99,6 +99,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port'+port)
 })
